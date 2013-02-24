c4677b2213ac18debb588ed511fb6a8f40b678fd
Luke <luketliu@gmail.com>
2013-02-23 18:46:10 -0600

    test env setup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,55 +2,59 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-	describe "Home page" do
+	let(:base_title) {"A New Twitter"}
+	let(:hone) {"Should have the h1"}
+	let(:tit) {"Should have the title"}
 
-		it "should have the h1 'Twitter Clone'" do
+	describe "Home page" do |hone, tit|
+
+		it "#{hone} 'Twitter Clone'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Twitter Clone')
 		end
 
-		it "should have the title 'Home'" do
+		it "#{tit} 'Home'" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => "A new Twitter | Home")
+			page.should have_selector('title', :text => "#{base_title} | Home")
 		end
 	end
 
-		describe "Help" do
+		describe "Help" do |hone, tit|
 
-		it "should have the h1 'Help'" do
+		it "#{hone} Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => 'Help')
 		end
 
-		it "should have the title 'Help'" do
+		it "#{tit} 'Help'" do
 			visit '/static_pages/help'
-			page.should have_selector('title', :text => "A new Twitter | Help")
+			page.should have_selector('title', :text => "#{base_title} | Help")
 		end
 	end
 
-		describe "About" do
+		describe "About" do |hone, tit|
 
-		it "should have the h1 'About'" do
+		it "#{hone} 'About'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About')
 		end
 
-		it "should have the title 'About'" do
+		it "#{tit} 'About'" do
 			visit '/static_pages/about'
-			page.should have_selector('title', :text => "A new Twitter | About")
+			page.should have_selector('title', :text => "#{base_title} | About")
 		end
 	end
 
-		describe "Contact Us" do
+		describe "Contact Us" do |hone, tit|
 
-		it "should have the h1 'Contact Us'" do
+		it "#{hone} Contact Us'" do
 			visit '/static_pages/contact'
 			page.should have_selector('h1', :text => 'Contact Us')
 		end
 
-		it "should have the title 'Contact Us'" do
+		it "#{tit} 'Contact Us'" do
 			visit '/static_pages/contact'
-			page.should have_selector('title', :text => "A new Twitter | Contact Us")
+			page.should have_selector('title', :text => "#{base_title} | Contact Us")
 		end
 	end
 
